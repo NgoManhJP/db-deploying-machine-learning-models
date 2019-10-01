292752492bb2d4b9a65afa29de9d816f42a2c5fc
NgoManhJP <ngovanmanh88@gmail.com>
2019-10-01 23:14:15 +0900

    特徴量

--- packages/regression_model/regression_model/processing/preprocessors.py
+++ packages/regression_model/regression_model/processing/preprocessors.py
@@ -146,35 +146,6 @@ class CategoricalEncoder(BaseEstimator, TransformerMixin):
         return X
 
 
-class LogTransformer(BaseEstimator, TransformerMixin):
-    """対数変換器"""
-
-    def __init__(self, variables=None):
-        if not isinstance(variables, list):
-            self.variables = [variables]
-        else:
-            self.variables = variables
-
-    def fit(self, X, y=None):
-        # パイプラインに対応するため
-        return self
-
-    def transform(self, X):
-        X = X.copy()
-
-        # 対数変換の値が負でないことを確認
-        if not (X[self.variables] > 0).all().all():
-            vars_ = self.variables[(X[self.variables] <= 0).any()]
-            raise ValueError(
-                f"Variables contain zero or negative values, "
-                f"can't apply log for vars: {vars_}")
-
-        for feature in self.variables:
-            X[feature] = np.log(X[feature])
-
-        return X
-
-
 class DropUnecessaryFeatures(BaseEstimator, TransformerMixin):
 
     def __init__(self, variables_to_drop=None):
